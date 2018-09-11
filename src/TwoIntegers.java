9016bc89c555d6c857df75015fb2be523f76843e
bob <bob@APCSA>
2018-09-11 15:40:29 -0700

    In progress.

---
+++ src/TwoIntegers.java
@@ -0,0 +1,55 @@
+public class TwoIntegers 
+{
+	private int num1;
+	private int num2;
+	
+	public TwoIntegers()
+	{
+		num1 = -1;
+		num2 = -1;
+	}
+	public TwoIntegers(int num1, int num2)
+	{
+		this.num1 = num1;
+		this.num2 = num2;
+	}
+	public int getNum1()
+	{
+		return num1;
+	}
+	public void setNum1(int num1)
+	{
+		this.num1 = num1;
+	}
+	public int getNum2()
+	{
+		return num2;
+	}
+	public void setNum2(int num2)
+	{
+		this.num2 = num2;
+	}
+	public String arithmetic(int sum, int product, int difference, int quotient)
+	{
+		sum = num1 + num2;
+		product = num1 * num2;
+		difference = num1 / num2;
+		quotient = num1 % num2;
+		
+		return num1 + "+" + num2 + "=" + sum + ", " 
+		+ num1 + "*" + num2 + "=" + product + ", " 
+		+ num1 + "/" + num2 + "=" + difference + ", " 
+		+ num1 + "%" + num2 + "=" + quotient + ", ";
+	}
+	public int larger()
+	{
+		if(num1 > num2)
+		{
+			return num1;
+		}
+		else
+		{
+			return num2;
+		}
+	}
+}
